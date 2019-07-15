7b1ce2f0b319d6e44d11943efb31ef32f25b59c3
GiurgiuDiana <48088089+GiurgiuDiana@users.noreply.github.com>
2019-07-15 15:35:21 +0300

    Add files via upload

---
+++ week2/Day1/test1/src/Calculator.java
@@ -0,0 +1,21 @@
+public  class Calculator {
+
+    public  int sumOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public  float divisionOfTwoNumbers(int a, int b) {
+        return (float) a / (float) b;
+    }
+
+    public  float multiplyTwoNumbers(int a, int b) {
+        return (float) a * b;
+    }
+
+    public  int subbTwoNumbers(int a, int b) {
+        return a - b;
+    }
+    public  Calculator(){}
+
+
+}
